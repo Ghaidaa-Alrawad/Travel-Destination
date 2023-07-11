277a102ff7c7e32442645a11e069b15f185b359d
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-07-11 19:06:35 +0000

    class16

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <h3>Author: Ghaidaa</h3>
+        <h5>
+          <a href="https://facebook.com">facebook.com</a>
+        </h5>
+      </footer>
+    );
+  }
+  
+  export default Footer;
